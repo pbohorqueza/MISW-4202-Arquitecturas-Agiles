7e060ea78b82174ba3d716343724b5ffd8fb99d4
Andres Garcia <oa.garcia2@uniandes.edu.co>
2024-02-25 20:03:07 -0500

    Agregando conexión remota a servideor de Redis cloud

--- monitor/monitor.py
+++ monitor/monitor.py
@@ -43,7 +43,7 @@ def logs():
 
 
 def run_monitor_ping_echo():
-    redis = Redis(configuracion['redis-host'], configuracion['redis-port'], configuracion['redis-db'])  # Conexión a Redis
+    redis = Redis(configuracion['redis-host'], configuracion['redis-port'], configuracion['redis-db'], configuracion['redis-password'])  # Conexión a Redis
     while True:
         print("Running monitor_ping_echo")
         
