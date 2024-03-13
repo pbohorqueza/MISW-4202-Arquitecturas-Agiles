5e7a16b5049ccfc9c3316e30af3b19d31f0a5e50
Andres Garcia <oa.garcia2@uniandes.edu.co>
2024-03-13 08:34:44 -0500

    renombrar carpeta a seguridad

--- experimento-seguridad/monitoreo-salud-deportista/configuracion.py
+++
@@ -1,5 +0,0 @@
-configuracion = {
-    'intervalo-peticiones-error': 5,
-    'entrenamiento-id': 1,
-    'entrenamiento-api-url': 'http://127.0.0.1:5002'
-}
--- experimento-seguridad/monitoreo-salud-deportista/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Corre el componente MonitoreoSaludDeportista
-export FLASK_APP=monitoreo-salud-deportista.py
-flask run -p 3001
-
-
