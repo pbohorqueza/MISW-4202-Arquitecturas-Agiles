e5fd39fca2bf758f028cda1de8d3a29dca5b2f13
Felipe Valencia <pipeedev@gmail.com>
2024-03-16 09:04:07 -0400

    chore: adding json results

--- experimento-seguridad/logger/main.py
+++ experimento-seguridad/logger/main.py
@@ -1,7 +1,7 @@
 import csv
 import logging
 import os
-
+import json
 import requests
 from dotenv import load_dotenv
 
@@ -13,8 +13,17 @@ GESTOR_DEPORTIVO_API_URL = os.environ.get('GESTOR_DEPORTIVO_API_URL')
 logging.basicConfig(filename='request_log.txt', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S')
 
-if __name__ == "__main__":
 
+def create_json_log(log):
+    with open('request_log.json', 'a') as file:
+        # write with double quotes
+        file.write(json.dumps(log))
+        file.write("\n")
+
+
+json_logs = []
+
+if __name__ == "__main__":
     with open('users.csv', 'r') as file:
         users = csv.DictReader(file)
 
@@ -32,13 +41,36 @@ if __name__ == "__main__":
                 if response.status_code == 200:
                     log = f"Endpoint {url} - Usuario {user['usuario']} - Status Code: {response.status_code}"
                     logging.info(log)
+                    json_log = {
+                        "endpoint": url,
+                        "usuario": user['usuario'],
+                        "status_code": response.status_code,
+                        "response": "OK"
+                    }
+                    json_logs.append(json_log)
                     print(log)
                 else:
                     log = f"Endpoint {url} - Usuario {user['usuario']} - Status Code: {response.status_code} - Response {response.text}"
+                    json_log = {
+                        "endpoint": url,
+                        "usuario": user['usuario'],
+                        "status_code": response.status_code,
+                        "response": response.json()['mensaje']
+                    }
+                    json_logs.append(json_log)
                     logging.info(log)
                     print(log)
 
             except requests.exceptions.RequestException as e:
                 logging.error(f"Login - Error: {e}")
                 print(f"Login - Error: {e}")
-                exit(1)
+                json_log = {
+                    "endpoint": url,
+                    "usuario": user['usuario'],
+                    "status_code": 500,
+                    "response": "Error"
+                }
+
+                json_logs.append(json_log)
+
+    create_json_log(json_logs)
