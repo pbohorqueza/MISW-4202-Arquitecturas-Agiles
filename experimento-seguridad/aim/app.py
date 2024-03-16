d409f6bfed3bdfd2f6c00662c30c4e4354e0a2b1
Andres Garcia <oa.garcia2@uniandes.edu.co>
2024-03-15 22:05:44 -0500

    creaciòn de BDD en el componente AIM

--- experimento-seguridad/aim/app.py
+++ experimento-seguridad/aim/app.py
@@ -2,16 +2,25 @@
 from flask import Flask, request, jsonify
 import jwt
 from typing import List, Dict, Optional
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
+db_path = os.path.join(os.path.dirname(__file__), 'usuarios.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_path
+db = SQLAlchemy(app)
+
 
 # Clave secreta para firmar y verificar el token (debería ser segura y secreta en un entorno real)
 SECRET_KEY = 'SPORTAPP_#20240301_MISO@JUPAAND'
 
 # Base de datos simulada con información de usuarios y sus credenciales
 credenciales_usuarios = [
-    {"uuid": "15893de-bf0c-4978-9880-53a84fe08b94", "usuario": "usuario1", "password": "password1"},
-    {"uuid": "488b2fc3-fb48-4549-8eaf-3c0ffca57c2a", "usuario": "usuario2", "password": "password2"}
+    {"uuid": "15893de-bf0c-4978-9880-53a84fe08b94", "usuario": "usuario1", "password": "password1", "reglas": "perfil-deportivo/deportista/salud"},
+    {"uuid": "488b2fc3-fb48-4549-8eaf-3c0ffca57c2a", "usuario": "usuario2", "password": "password2", "reglas": "perfil-demografico/deportista/pais"},
+    {"uuid": "ce5b2145-5e00-464f-81b7-484152955bcd", "usuario": "usuario3", "password": "password3", "reglas": "entrenamiento/deportista/estado-fisico"},
+    {"uuid": "e5aa3d12-15e3-4d04-830c-25277c959e4b", "usuario": "usuario4", "password": "password4", "reglas": "facturacion/socio/logistica"},
+    {"uuid": "9f64e7c3-e89d-4b49-bb43-0d3a1107b83a", "usuario": "usuario5", "password": "password5", "reglas": "perfil-deportivo/deportista/salud"}
 ]
 
 @app.route('/')
@@ -31,14 +40,19 @@ def validar_token():
     if len(partes) != 2 or partes[0].lower() != 'bearer':
         return jsonify({"mensaje": "Encabezado de autorización inválido"}), 401
 
-    # Obtener el token de los parts
+    # Obtener el token de las partes
     token = partes[1]
 
     # Decodificar y verificar el token
     try:
         token_payload = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
         usuario_id = token_payload.get('uuid')
-        return jsonify({"uuid": usuario_id}), 200
+        usuario = Usuario.query.filter_by(uuid=usuario_id).first()
+        
+        if usuario:
+            return jsonify({"uuid": usuario_id, "reglas": usuario.reglas}), 200
+        else:
+            return jsonify({"mensaje": "Usuario inválido"}), 401 
     except jwt.ExpiredSignatureError:
         return jsonify({"mensaje": "Token expirado"}), 401
     except jwt.InvalidTokenError:
@@ -68,3 +82,33 @@ def verificar_credenciales(usuario: str, password: str, usuarios: List[Dict[str,
     return usuario
 
 
+
+
+class Usuario(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    uuid = db.Column(db.String(36), unique=True, nullable=False)
+    usuario = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(50), nullable=False)
+    reglas = db.Column(db.String(50), unique=False, nullable=False)
+
+    def _repr_(self):
+        return '<Usuario %r>' % self.usuario
+
+# Ruta para consultar un usuario por nombre y contraseña
+@app.route('/consulta_usuario', methods=['POST'])
+def consulta_usuario():
+    data = request.json
+    usuario = data.get('usuario')
+    password = data.get('password')
+    
+    usuario_encontrado = Usuario.query.filter_by(usuario=usuario, password=password).first()
+    
+    if usuario_encontrado:
+        return jsonify({'mensaje': 'Usuario encontrado'})
+    else:
+        return jsonify({'mensaje': 'Usuario no encontrado'})
+
+
+
+with app.app_context():
+    db.create_all()
